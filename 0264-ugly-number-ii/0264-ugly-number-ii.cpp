2067a904f6e568081197f083d079b86169ada959
Jobanvir1207 <68375832+Jobanvir1207@users.noreply.github.com>
2022-10-17 12:45:51 +0530

    Time: 11 ms (80.71%), Space: 9.7 MB (34.23%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+     vector <int> results (1,1);
+        int i = 0, j = 0, k = 0;
+        while (results.size() < n)
+        {
+            results.push_back(min(results[i] * 2, min(results[j] * 3, results[k] * 5)));
+            if (results.back() == results[i] * 2) ++i;//if the number we got is same as we just pushed in the vector
+            if (results.back() == results[j] * 3) ++j;
+            if (results.back() == results[k] * 5) ++k;
+        }
+        return results.back();
+      //i j k will only increment if they are added else not
+    }
+};
